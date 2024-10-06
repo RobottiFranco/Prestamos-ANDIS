4b68f0ff46ff7a07a91f54a5873b3f539214d32f
JP206 <88661539+JP206@users.noreply.github.com>
2024-10-06 19:32:18 -0300

    arreglo RateLimitingInterceptor

--- src/main/java/com/andis/prestamos/interceptor/RateLimitingInterceptor.java
+++ src/main/java/com/andis/prestamos/interceptor/RateLimitingInterceptor.java
@@ -18,14 +18,14 @@ public class RateLimitingInterceptor implements HandlerInterceptor {
     @Autowired
     private CacheManager cacheManager;
 
-    private static final int LIMIT = 3; // Número máximo de solicitudes permitidas
-    private static final long TIME_WINDOW = 30000; // Ventana de tiempo en milisegundos (60 segundos)
+    private static final int LIMIT = 3; // solicitudes concurrentes maximas
+    private static final long TIME_WINDOW = 30000; // tiempo de ventana (30 segundos)
 
     @SuppressWarnings("unchecked")
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("Rate limiting interceptor triggered for: " + request.getRemoteAddr());
-        String clientIp = request.getRemoteAddr(); // Obtener la IP del cliente
+        String clientIp = request.getRemoteAddr();
         Cache cache = cacheManager.getCache("rateLimitCache");
         List<Long> requestTimes = cache.get(clientIp, List.class);
 
@@ -36,18 +36,15 @@ public class RateLimitingInterceptor implements HandlerInterceptor {
         long now = System.currentTimeMillis();
         requestTimes.add(now);
 
-        // Eliminar solicitudes que estén fuera de la ventana de tiempo
         requestTimes.removeIf(time -> now - time > TIME_WINDOW);
 
-        // Si el número de solicitudes excede el límite, devolver error 429
         if (requestTimes.size() > LIMIT) {
             response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
             response.getWriter().write("Rate limit exceeded. Please try again later.");
-            return false;  // Bloquear la solicitud
+            return false;
         }
 
-        // Actualizar el caché con las nuevas marcas de tiempo
         cache.put(clientIp, requestTimes);
-        return true;  // Continuar con el manejo de la solicitud
+        return true;
     }
 }
